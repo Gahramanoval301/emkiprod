584d4da880eecacb9d141b8783ca5054d1470d3b
Isko2003 <theazeisko2003@gmail.com>
2024-08-07 15:02:53 +0400

    tailwind elave olundu

--- emkiprod-react/src/App.jsx
+++ emkiprod-react/src/App.jsx
@@ -6,17 +6,35 @@ import Events from './Components/Events';
 import Blogs from './Components/Blogs';
 import Sponsors from './Components/Sponsors';
 import About from './Components/About';
+import logo from './Images/logo.png';
 function App() {
   return (
    <div>
-        <div className='navbar'>
+    <div className='header flex justify-around'>
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+      <div className='flex h-16 gap-3 navbar'>
+        <div className='flex gap-3 h-12 w-full pt-3 nav'>
           <Link to={'/'}>Home</Link>
           <Link to={'/events'}>Events</Link>
           <Link to={'/blogs'}>Blogs</Link>
           <Link to={'/sponsors'}>Sponsors</Link>
           <Link to={'/about'}>About Us</Link>
         </div>
-
+      </div>
+      <div className='flex gap-3'>
+        <select name="" id="select">
+          <option value="EN">EN</option>
+          <option value="AZ">AZ</option>
+          <option value="RU">RU</option>
+        </select>
+        <div>
+          <button className='bg-white text-black'>Buy ticket</button>
+        </div>
+      </div>
+    </div>
+        <p className='font-bold text-3xl text-amber-600'>Tailwind</p>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/events' element={<Events />}/>
