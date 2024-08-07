79af787d8d1c80936e1c8425e5b2c247bf926893
Isko2003 <theazeisko2003@gmail.com>
2024-08-07 13:43:01 +0400

    redux toolkit elave olundu

---
+++ emkiprod-react/src/Redux/Slices/counterSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    value: 0,
+}
+
+export const counterSlice = createSlice({
+    name: '',
+    initialState,
+    reducers: {}
+})
+
+export const { } = counterSlice.actions;
+export default counterSlice.reducer;
