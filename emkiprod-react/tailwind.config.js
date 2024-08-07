ff8e15a482e401b621b6b1643adb4cd38e0eaa74
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-07 17:41:09 +0400

    corrections on folder structure

--- emkiprod-react/tailwind.config.js
+++ emkiprod-react/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary:'#101426',
+        secondary: '#151A30',
+        white: '#F7F9FC',
+        pink:"#E32682",
+      }
+    },
   },
   plugins: [],
 }
