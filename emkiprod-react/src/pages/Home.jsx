dc92a0b6120c92f6d2709803bc5734ea2ea7ee1d
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-08 14:38:25 +0400

    adding discover events seciton to the home page

--- emkiprod-react/src/pages/Home.jsx
+++ emkiprod-react/src/pages/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import PageContainer from "../components/common_/PageContainer";
 import HomeBlogs from "../components/HomeBlogs";
 import SponsorRow from "../components/SponsorRow";
+import DiscoverEvents from "../components/DiscoverEvents";
 
 const Home = () => {
   return (
     <PageContainer>
+      <DiscoverEvents />
       <HomeBlogs />
       <SponsorRow />
     </PageContainer>
