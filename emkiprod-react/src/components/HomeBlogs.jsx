3c6653c4686f8de452b006d6bf1b22e1db817235
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-08 20:48:36 +0400

    optimization and correcting layout margin/container

--- emkiprod-react/src/components/HomeBlogs.jsx
+++ emkiprod-react/src/components/HomeBlogs.jsx
@@ -5,7 +5,7 @@ import BlogsSlider from "./BlogsSlider";
 function HomeBlogs() {
   return (
     <section id="blogs">
-      <div className="mx-auto w-11/12 sm:w-4/5  mb-6 xl:mb-0 pt-16 sm:pt-16 md:pt-16 xl:pt-16 pb-0 sm:pb-0 md:pb-0 xl:pb-0">
+      <div className="mx-auto w-11/12 sm:w-4/5 section">
         <SectionTitle>Blogs</SectionTitle>
         <BlogsSlider />
       </div>
