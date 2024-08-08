21418dbbf76770bc1689a2d716538f317ddea163
Elvin <elvinsrkrov@gmail.com>
2024-08-08 16:32:45 +0400

    valueCard component and valuesData have created

---
+++ emkiprod-react/src/components/ValueCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import line from "../../public/assets/line.png";
+
+function ValueCard({ value }) {
+  return (
+    <div className="flex items-center h-52 box">
+      <div>
+        <img src={value.image} alt="check" width={"156px"} height={"156px"} />
+      </div>
+      <div>
+        <h3>{value.title}</h3>
+        <div className="line">
+          <img src={line} alt="line" />
+        </div>
+        <p className="w-60">{value.content}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ValueCard;
---
+++ emkiprod-react/src/data/sectionDatas.js
@@ -0,0 +1,31 @@
+import imageFirst from "../../public/assets/rectangle1.svg";
+import imageSecond from "../../public/assets/rectangle2.svg";
+import imageThird from "../../public/assets/rectangle3.svg";
+import imageFourth from "../../public/assets/rectangle4.svg";
+
+export const valuesSectionData = [
+  {
+    id: 1,
+    title: "Trust",
+    content:"We always give frank feedback and actionable advice to help you seethe big picture.",
+    image: imageFirst,
+  },
+  {
+    id: 2,
+    title: "Transparency",
+    content:" We help you understand things clearly by removing all the confusing information",
+    image: imageSecond,
+  },
+  {
+    id: 3,
+    title: "Innovation",
+    content:"Our events have been meticulously built and match the ambition of modern businesses.",
+    image: imageThird,
+  },
+  {
+    id: 4,
+    title: "Collaboration",
+    content:"We approach every engagement with confidence in our ability to forge a long-term relationships.",
+    image: imageFourth,
+  },
+];
