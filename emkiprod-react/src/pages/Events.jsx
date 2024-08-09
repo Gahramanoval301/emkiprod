5632edec016bd33db49e23eb85e3bf25962b2085
Isko2003 <theazeisko2003@gmail.com>
2024-08-09 08:15:39 +0400

    events sayti bitdi

--- emkiprod-react/src/pages/Events.jsx
+++ emkiprod-react/src/pages/Events.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import PageContainer from '../components/common_/PageContainer'
+import { discoverEventsData } from '../data/discoverEvents'
 
 const Events = () => {
   return (
     <PageContainer>
-      <div>Events</div>
+      <h3 className='text-white flex justify-center w-96 text-2xl h-24 items-end'>Events</h3>
+      <div className='grid grid-cols-3 m-auto events-section place-content-center'>
+        {discoverEventsData.map(({id, title, thumbnail, location, price, date, ticketURL}) => {
+          return (
+            <div className='section hover:cursor-pointer w-[450px] section-reused'>
+              <div className='h-[380px] event-box'>
+                <img src={thumbnail} alt={`${title}'s concert`} className='object-contain rounded-3xl' width={"360px"} height={"360px"}/>
+                <div className='overlay'>
+                  <a href={ticketURL} target='_blank'><button>Read More</button></a>
+                </div>
+              </div>
+              <div className='border-[1px] rounded-3xl border-primary-light py-2 px-5  text-white_  hover:bg-primary-light event'>
+                  <div className='flex justify-between font-semibold my-2'>
+                    <p className='font-extrabold text-xl'>{title}</p>
+                    <p className='text-pink'>{price}</p>
+                  </div>
+                  <div className='flex justify-start gap-1 my-2 text-sm'>
+                    <p>{date}</p>
+                    <span>&bull;</span>
+                    <p>{location}</p>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </PageContainer>
   )
 }
