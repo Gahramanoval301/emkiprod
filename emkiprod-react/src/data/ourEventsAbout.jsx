bab80edb8c8d88c56c6fc5e4256a103906a390a7
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-10 14:57:51 +0400

    writing data for the events of about section

---
+++ emkiprod-react/src/data/ourEventsAbout.jsx
@@ -0,0 +1,25 @@
+export const aboutEventsData = [{
+    id:0,
+    image:"/assets/about_1.png",
+    title:"Thomas Anders"
+},{
+    id:1,
+    image:"/assets/about_2.png",
+    title:"ALAN WALKER"
+},{
+    id:2,
+    image:"/assets/about_3.jpg",
+    title:"ALAN WALKER"
+},{
+    id:3,
+    image:"/assets/about_4.jpg",
+    title:"Thomas Anders"
+},{
+    id:4,
+    image:"/assets/about_5.webp",
+    title:"Thomas Anders"
+},{
+    id:5,
+    image:"/assets/about_1.png",
+    title:"Thomas Anders"
+}]
