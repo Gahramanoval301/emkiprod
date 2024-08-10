f84e8a7953694fec76b1fe93a38de2a06316cefc
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-10 13:14:46 +0400

    creating initial structure of our events section in project

---
+++ emkiprod-react/src/components/AboutUs/OurEvents.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import OurEventsSwiper from './Swiper'
+
+const OurEvents = () => {
+  return (
+      <div className='section text-white_'>
+          <h2 className='text-3xl font-semibold'>Our Events</h2>
+          <OurEventsSwiper/>
+    </div>
+  )
+}
+
+export default OurEvents
