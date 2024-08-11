ffdb5119301bbfc8ed0c0d7fb89acc0093e185f2
Elvin <elvinsrkrov@gmail.com>
2024-08-11 18:38:20 +0400

    footer and projectRow responsive design has added

--- emkiprod-react/src/components/Footer.jsx
+++ emkiprod-react/src/components/Footer.jsx
@@ -9,13 +9,13 @@ const Footer = () => {
     <section id="footer">
       <div className="border-[1px] border-[#222b45] border-style mt-[80px]">
         <div className="mx-auto w-11/12 sm:w-4/5 xl:mb-0 pt-12 sm:pt-16 md:pt-16 xl:pt-16 pb-2 sm:pb-2 md:pb-2 xl:pb-2  MuiBox-root css-0">
-          <div className="flex align-start justify-between">
+          <div className="flex md:flex-row flex-col md:items-start items-center justify-between">
             <div className="footer-logo">
               <img src="/assets/logo-medium.png" alt="footer logo" />
             </div>
-            <div className="flex justify-evenly w-full">
+            <div className="flex justify-evenly w-full max-[768px]:mt-12 max-[768px]:flex-wrap max-[768px]:gap-9 max-[768px]:mt-12 max-[640px]:justify-between">
               <div className="footer-column">
-                <h3 className="text-[#f5f5f6] text-base mb-5 font-medium">
+                <h3 className="text-[#f5f5f6] sm:text-base text-sm mb-5 font-medium">
                   {t("common.social")}
                 </h3>
                 <Link to="" target="_blank">
@@ -25,7 +25,7 @@ const Footer = () => {
                       className="mr-2 w-4"
                       alt="facebook"
                     />
-                    <span className="text-[#E4E9F2] text-base font-normal">
+                    <span className="text-[#E4E9F2] sm:text-base text-sm font-normal">
                       {t("common.facebook")}
                     </span>
                   </div>
@@ -37,39 +37,39 @@ const Footer = () => {
                       className="mr-2 w-4"
                       alt="instagram"
                     />
-                    <span className="text-[#E4E9F2] text-base font-normal">
+                    <span className="text-[#E4E9F2] sm:text-base text-sm font-normal">
                       {t("common.instagram")}
                     </span>
                   </div>
                 </Link>
               </div>
               <div className="footer-column">
-                <h3 className="text-[#f5f5f6] text-base mb-5 font-medium">
+                <h3 className="text-[#f5f5f6] sm:text-base text-sm mb-5 font-medium">
                   {t("common.information")}
                 </h3>
                 <Link to="">
                   <div className="flex mb-3">
-                    <span className="text-[#E4E9F2] text-base font-normal">
+                    <span className="text-[#E4E9F2] sm:text-base text-sm font-normal">
                       {t("common.aboutus")}
                     </span>
                   </div>
                 </Link>
                 <Link to="">
                   <div className="flex mb-3">
-                    <span className="text-[#E4E9F2] text-base font-normal">
+                    <span className="text-[#E4E9F2] sm:text-base text-sm font-normal">
                       {t("common.sponsor")}
                     </span>
                   </div>
                 </Link>
               </div>
               <div className="footer-column">
-                <h3 className="text-[#f5f5f6] text-base mb-5 font-medium">
+                <h3 className="text-[#f5f5f6] sm:text-base text-sm mb-5 font-medium">
                   {t("common.contact")}
                 </h3>
                 <Link href="" target="_blank">
                   <div className="flex mb-3">
                     <img src="/assets/sms.svg" className="mr-2 w-4" alt="sms" />
-                    <span className="text-[#E4E9F2] text-base font-normal">
+                    <span className="text-[#E4E9F2] sm:text-base text-sm font-normal">
                       office@emki.az
                     </span>
                   </div>
--- emkiprod-react/src/components/ProjectImg.jsx
+++ emkiprod-react/src/components/ProjectImg.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function SponsorImg() {
   return (
-    <div className="ml-[40px]">
+    <div className="xl:ml-[40px] max-[1300px]:mt-[40px]">
       <img
         src="/assets/sponsor-side-img.png"
         alt="sponsor side img"
-        className="rounded-2xl h-min max-w-[490px]"
+        className="rounded-2xl h-min lg:max-w-[490px] max-[992px]:max-w-[380px] max-[640px]:max-w-[340px] max-w-[450px] shadow-xl"
       />
     </div>
   );
