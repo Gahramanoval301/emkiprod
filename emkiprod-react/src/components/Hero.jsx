619841c1adee02019c20f36fb2bd37a3796ca97e
emilhuseynvh <emilhuseynvh@gmail.com>
2024-08-11 15:26:16 +0400

    hero button

--- emkiprod-react/src/components/Hero.jsx
+++ emkiprod-react/src/components/Hero.jsx
@@ -3,8 +3,10 @@ import HeroSliderDesktop from "./HeroSliderDesktop";
 import HeroSliderMobile from "./HeroSliderMobile";
 import { FaCircleArrowRight } from "react-icons/fa6";
 import { useTranslation } from 'react-i18next';
+import { Navigate, useNavigate } from "react-router";
 
 function Hero() {
+    const navigate = useNavigate();
     const { t} = useTranslation();
     return (
       <div className='hero text-white'>
@@ -12,7 +14,7 @@ function Hero() {
                         <div className='sm:w-[100%] mt-11 pt-14'>
                     <h1 className='text-4xl md:text-7xl font-semibold  whitespace-normal mb-10'>{t("home.elevateyourconcertexperience")}</h1>
                     <p className='whitespace-normal text-base font-normal mb-7'>{t("home.welcometotheultimatedestinationformusicenthusiastsandconcertgoersatemkiproductionwerepassionateaboutbringingyoutheverybestinlivemusicexperienceswhetheryoureadiehardfanoracasuallistenerwevegotsomethingspecialinstoreforyou")}</p>
-                            <button className='bg-white_ text-primary-default font-semibold tracking-wider px-8 py-2 rounded-lg align-middle text-primary flex gap-3 items-center hover:bg-[#e32682] hover:text-[#fff] transition-all duration-500'>Events <FaCircleArrowRight />  </button>
+                            <button onClick={() => navigate('/events')}  className='bg-white_ text-primary-default font-semibold tracking-wider px-8 py-2 rounded-lg align-middle text-primary flex gap-3 items-center hover:bg-[#e32682] hover:text-[#fff] transition-all duration-500'>Events <FaCircleArrowRight />  </button>
                         </div>
                         <div className='w-4/5'>
                             <HeroSliderDesktop />
