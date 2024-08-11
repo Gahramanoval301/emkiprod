ba8c339698235935731f47c73f25827298845857
Isko2003 <theazeisko2003@gmail.com>
2024-08-11 13:40:07 +0400

    dropdown duzeldildi

--- emkiprod-react/src/components/Header.jsx
+++ emkiprod-react/src/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
 
     return (
         <div>
-            <header onClick={() => { !flag && setFlag(!flag) }} className='w-full text-white_ '>
+            <heaader onClick={() => { !flag && setFlag(!flag) }} className='w-full text-white_ '>
                 <nav className='flex bg-primary-default lg:w-[90%] lg:h-auto h-[70px] w-full m-auto justify-between bg-primary py-6 px-8 z-[999] top-0 right-0 fixed lg:relative  rounded-none items-center lg:rounded-bl-24 lg:rounded-br-24'>
                     <div onClick={() => setShow(!show)} className='flex lg:hidden flex-col gap-[6px] hamburger cursor-pointer'>
                         <img className={`select-none transition-transform duration-300 ${!show ? 'rotate-45 translate-y-2' : 'rotate-0'}`} src={rectangle} />
@@ -35,18 +35,6 @@ const Header = () => {
                         <Link className='mx-3 hover:text-[#e32682] transition-all duration-500' to='/about'>{t("navbar.aboutus")}</Link>
                     </div>
                     <LanguageSelector/>
-                    {/* <div className='flex items-center'>
-                        <div className='relative hidden lg:block'>
-                            <div className='flex items-center text-base cursor-pointer select-none' onClick={() => setFlag(!flag)}>{lang[index]}{flag ? <FaChevronDown className='w-3 mx-2' /> : <FaChevronUp className='w-3 mx-2' />}</div>
-                            <div style={flag ? { display: 'none' } : { display: 'block' }} className='absolute top-full mt-2  text-white_ bg-secondary z-50'>
-                                {lang.map((elem, i) => ((i !== index) && <div key={i} className='text-base cursor-pointer px-4 py-2 hover:bg-[#111526]' onClick={() => { setIndex(i); setFlag(!flag) }}>{elem}</div>))}
-                            </div>
-                        </div>
-                        <div className='text-[#101426] font-medium text-xl ml-6 hover:text-[#fff]  transition-all duration-500 headerBuyTicket'>
-                            <button className='bg-white px-8 py-2 rounded-lg align-middle hidden lg:inline-block'>Buy Ticket</button>
-                            <button className='bg-white px-2 py-2 rounded-lg align-middle mx-[1px]'><IoTicket className='w-5 h-7' /></button>
-                        </div>
-                    </div> */}
                 </nav>
                 {/*  H A M B U R G E R   M E N U  */}
                 <div className={`xs:w-64 w-full pt-16 h-full px-4 mt-14 bg-white fixed z-[500] ${show && 'close'} duration-500`}>
@@ -66,7 +54,7 @@ const Header = () => {
                     </div>
                 </div>
 
-            </header>
+            </heaader>
         </div>
     )
 }
