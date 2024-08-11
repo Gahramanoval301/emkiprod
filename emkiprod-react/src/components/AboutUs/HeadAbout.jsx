13182345b1c64919a33ee60aad311d4aa6f2232f
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-11 13:55:17 +0400

    coorecting

--- emkiprod-react/src/components/AboutUs/HeadAbout.jsx
+++ emkiprod-react/src/components/AboutUs/HeadAbout.jsx
@@ -8,7 +8,7 @@ const HeadAbout = ({t}) => {
               <div className=''>
                   <h3 className='text-5xl leading-[1.4]'>{t("about.unleashyoureventspotential")}</h3>
                   <h2 className='text-[50px] text-border mt-2'>{t("about.elevateyourstreamingwithourexpertise")}</h2>
-                  <p className='my-5 text-xl text-justify leading-relaxed'>{t("home.showyoureventtotheworldthroughanyplatformweareleadersinstreamingandbroadcastservicesforeventsofferingconsultingservicesanddevelopmentofcompletesystemsforourclientsineventbroadcasting")}</p>
+                  <p className='my-5 text-xl text-justify leading-relaxed'>{t("home.sponsorText")}</p>
               </div>
               <div className='flex justify-center items-center'>
                   <video src="" poster="/assets/video_thumbnail.png" controls width="100%" height="100%"></video>
