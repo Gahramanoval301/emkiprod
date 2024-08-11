b3d10c7eaf75e7fc5058a4c64cfd1c23bbd30323
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-11 16:50:09 +0400

    updating language selector functionality and designs based on mobile devices

--- emkiprod-react/src/components/LanguageSelector.jsx
+++ emkiprod-react/src/components/LanguageSelector.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
-import { IoTicket } from 'react-icons/io5';
 
-const LanguageSelector = () => {
+const LanguageSelector = ({isMobileNavbar}) => {
     const { t, i18n } = useTranslation();
     const [flag, setFlag] = useState(true);
     const [index, setIndex] = useState(0);
@@ -21,12 +20,12 @@ const LanguageSelector = () => {
 
     return (
         <div className='flex items-center'>
-            <div className='relative hidden lg:block'>
-                <div className='flex items-center text-base cursor-pointer select-none' onClick={() => setFlag(!flag)}>
+            <div className={`relative ${isMobileNavbar ? "block" : "hidden"} ${isMobileNavbar ? "lg:hidden" : "lg:block"} `}>
+                <div className={`flex items-center text-base cursor-pointer select-none ml-3 p-1 ${isMobileNavbar && "text-primary-default border-2"}`} onClick={() => setFlag(!flag)}>
                     {langNames[languages[index]]}
-                    {flag ? <FaChevronDown className='w-3 mx-2' /> : <FaChevronUp className='w-3 mx-2' />}
+                    {flag ? <FaChevronDown className={`w-3 mx-2 ${isMobileNavbar && "text-primary-default"}`} /> : <FaChevronUp className={`w-3 mx-2 ${isMobileNavbar && "text-primary-default"}`} />}
                 </div>
-                <div style={flag ? { display: 'none' } : { display: 'block' }} className='absolute top-full mt-2 text-white bg-secondary z-50'>
+                <div style={flag ? { display: 'none' } : { display: 'block' }} className='absolute top-full mt-2 ml-3 text-white bg-secondary z-50'>
                     {languages.map((lng, i) => (
                         i !== index && (
                             <div key={i} className='text-base cursor-pointer px-4 py-2 hover:bg-[#111526]' onClick={() => { setIndex(i); handleLanguageChange(lng); }}>
