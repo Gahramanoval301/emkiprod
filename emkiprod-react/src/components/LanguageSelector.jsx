e6e93f140c8fa34f6a4cb50e8dd8dbecd650f642
emilhuseynvh <emilhuseynvh@gmail.com>
2024-08-11 18:51:27 +0400

    language selector

--- emkiprod-react/src/components/LanguageSelector.jsx
+++ emkiprod-react/src/components/LanguageSelector.jsx
@@ -14,8 +14,8 @@ const LanguageSelector = ({isMobileNavbar}) => {
     };
 
     const handleLanguageChange = (lng) => {
+        setFlag(true);
         i18n.changeLanguage(lng);
-        setFlag(false);
     };
 
     return (
