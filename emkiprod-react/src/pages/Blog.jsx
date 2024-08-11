0430bd16463fad9894313cbdf5557157e53a8762
emilhuseynvh <emilhuseynvh@gmail.com>
2024-08-11 16:04:45 +0400

    blog

--- emkiprod-react/src/pages/Blog.jsx
+++ emkiprod-react/src/pages/Blog.jsx
@@ -23,7 +23,7 @@ function Blog() {
         <PageContainer>
             <div className="m-auto w-11/12 sm:w-4/5 pt-24 xl:mb-0 sm:pt-24 md:pt-20 xl:pt-14">
                 <img className="rounded-[28px] w-full h-auto" src={url} alt="Image" />
-                <h1 className="text-xl md:text-4xl text-white mb-16 mt-10">{titles[id]}</h1>
+                <h1 className="text-xl md:text-4xl text-white mb-16 mt-10">{titles[id - 1]}</h1>
                 {updatedDesc.map((text, i) => <p key={i} className="text-white my-6 text-sm md:text-base">{text}</p>)}
             </div>
         </PageContainer>
