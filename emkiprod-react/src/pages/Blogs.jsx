8b82cf1a13768a510f52a0deb6aae2e515c60899
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-20 15:22:15 +0400

    giving blogs api data

--- emkiprod-react/src/pages/Blogs.jsx
+++ emkiprod-react/src/pages/Blogs.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageContainer from "../components/common_/PageContainer";
 import BlogCard from "../components/BlogCard";
 import { useBlogsData } from "../hooks/useBlogsData";
+import { useTranslation } from "react-i18next";
+import { getAllPageData } from "../api/data";
 
 const Blogs = () => {
   const { blogData, t } = useBlogsData();
+  const [pageData, setPageData] = useState([]);
+  const { i18n } = useTranslation();
+
+  const getLanguageId = () => {
+    switch (i18n.language) {
+      case "az":
+        return 1
+      case "en":
+        return 2;
+      case "ru":
+        return 3;
+    }
+  }
+
+  useEffect(() => {
+    getAllPageData().then((data) => {
+      setPageData(data);
+    })
+  }, [])
+  
+  const currentLanguageId = getLanguageId()
+  const filteredPageData = pageData.find((data) => data.id === currentLanguageId)
+  console.log(filteredPageData && filteredPageData.postTranslations);
+  
 
   return (
     <PageContainer>
