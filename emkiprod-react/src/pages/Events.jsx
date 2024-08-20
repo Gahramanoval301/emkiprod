ae2a92ebe1202dd447a561457a13bb3bb1bdc4e4
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-20 15:22:48 +0400

    events section data

--- emkiprod-react/src/pages/Events.jsx
+++ emkiprod-react/src/pages/Events.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageContainer from "../components/common_/PageContainer";
 import EventCard from "../components/EventCard";
 import { useEventsData } from "../hooks/useEventsData";
+import { getAllPageData, getAnnouncementTranslationsId } from "../api/data";
+import { useTranslation } from "react-i18next";
 
 const Events = () => {
-  const { eventsData, t } = useEventsData();
+  // const { eventsData, t } = useEventsData();
+  const { t, i18n } = useTranslation();
+
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+
+    const currentLanguage = i18n.language;
+    console.log(currentLanguage);
+
+    const getLanguageId = () => {
+      switch (currentLanguage) {
+        case "en":
+          return 6;
+        // filteredData = data.filter(({ languageId }) => languageId == 6);
+        case "ru":
+          return 7;
+        // filteredData = data.filter(({ languageId }) => languageId == 7);
+        case "az":
+          // filteredData = data.filter(({ languageId }) => languageId == 5);
+          return 5;
+        default:
+          break;
+      }
+    };
+    const langId = getLanguageId()
+    getAnnouncementTranslationsId(langId).then((data) => {
+      setData(data);
+    });
+  }, []);
+  console.log(data);
+
 
   return (
     <PageContainer>
@@ -12,7 +46,7 @@ const Events = () => {
         {t("events.event")}
       </h3>
       <div className="grid m-auto events-section place-content-center xs:block md:block lg:grid grid-cols-3 xl:grid xl:grid-cols-3">
-        {eventsData.map((event) => (
+        {data.map((event) => (
           <EventCard key={event.id} event={event} t={t}/>
         ))}
       </div>
